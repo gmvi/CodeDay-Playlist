7fd75b5a90d0deb4919c594ae460e0e6d2e5cacc
George Matter VI <George.Matter.VI@gmail.com>
2013-05-30 23:52:07 -0700

    switch to VLCClient, new queuing system, more
    * Switched program.py to libvlc rather than telnet
    * Removed vlc telnet shit
    * New queuing system adds a song when current song is so many seconds
    from ending
    * In program.py, shut_down() is now global, for easier debugging
    * Refactored handling of transferred functions. Now it's more logical,
    but fucking ugly.

--- program.py
+++ program.py
@@ -1,4 +1,5 @@
-from vlcclient import VLCClient
+from vlc import State
+from libvlc_controller import VLCController
 import os, time, threading, webpage, subprocess
 from socket import error as socketerror
 from util import FORMATS, Artist, Track, bufferlist
@@ -7,56 +8,26 @@ base = os.path.abspath(os.path.curdir)
 
 ARTIST_BUFFER_SIZE = 4
 SONG_BUFFER_SIZE = 30
-VLC_LOC = "C:\\Program Files\\videolan\\vlc"
-AUTO_START_VLC
+#VLC_LOC = "C:\\Program Files\\videolan\\vlc"
+TIME_CUTOFF_MS = 3000
+LOOP_PERIOD_SEC = 2
+DEBUG = True
 
-vlc = None
-vlc_process = None
+v = None
 db = None
+music_thread = None
+webpage_thread = None
 artist_buffer = bufferlist(ARTIST_BUFFER_SIZE)
 song_buffer = bufferlist(SONG_BUFFER_SIZE)
 
 def load_vlc():
-    global vlc, vlc_process
-    vlc = VLCClient("::1")
-    first = True
-    while True:
-        try:
-            vlc.connect()
-            break
-        except socketerror:
-            vlc.disconnect()
-            if AUTO_START_VLC and first:
-                try:
-                    vlc_process = subprocess.Popen("\"%s\\vlc\" --intf telnet" % VLC_LOC)
-                except Exception as e:
-                    print "Error trying to start VLC"
-                    print e
-                first = False
-                time.sleep(1)
-            else:
-                raw_input("Cannot connect to VLC. [ENTER] to try again.")
-    vlc.clear()
-    vlc.volume(100)
+    global v
+    v = VLCController()
 
-def play():
-    if status[2] != 'playing': vlc.play()
-
-def pause():
-    if status[2] == 'playing': vlc.pause()
-
-def status():
-    ret = [None, None, None]
-    string = vlc.status().split('\r\n')
-    if len(string) == 3:
-        ret[0] = string[0][13:-2]
-    ret[1] = int(string[-2][16:-2])
-    ret[2] = string[-1][8:-2]
-    return tuple(ret)
-
-def add(path):
-    vlc.add(os.path.join(base, path))
-    vlc.play()
+def should_add_another():
+    media = v.media_player.get_media()
+    return media is not None and \
+           media.get_duration() - v.media_player.get_time() < TIME_CUTOFF_MS
 
 def get_all(path):
     ret = []
@@ -65,7 +36,8 @@ def get_all(path):
         if os.path.isdir(node):
             ret += get_all(node)
         else:
-            ret.append(node)
+            if os.path.splitext(node)[1] in FORMATS:
+                ret.append(node)
     return ret
 
 def load_database(path):
@@ -79,7 +51,10 @@ def load_database(path):
         else:
             artist = Artist(direc)
             for track in get_all(node):
-                artist.add(Track(track))
+                try:
+                    artist.add(Track(track))
+                except IOError:
+                    pass
             db.append(artist)
     unknown_artist = Artist("")
     for track in unknown:
@@ -104,33 +79,44 @@ def choose():
 
 RUNNING = True
 def run():
+    v.add(choose().path)
+    v.play()
+    while v.media_player.get_state() != State.Playing:
+        pass
     while RUNNING:
-        if status()[2] != 'playing':
-            add(choose().path)
-        time.sleep(2)
+        if should_add_another():
+            v.add(choose().path)
+            if v.media_player.get_state() != State.Playing:
+                v.play_last()
+        time.sleep(LOOP_PERIOD_SEC)
 
-def get_track(): # Update to check database. Also update database to make this possible.
-    if status()[2] == 'playing':
+def get_track(): # Update to get from playlist and compare to db.
+    if v.media_player.get_state() == State.Playing:
         if len(song_buffer) > 0:
             return song_buffer[-1]
 
+
+def shut_down():
+    music_thread._Thread__stop()
+    webpage_thread._Thread__stop()
+
 def main():
+    global music_thread, webpage_thread
     load_vlc()
     path = raw_input("music: ")
     print "Constructing Database..."
     load_database(path)
-    webpage_thread = threading.Thread(target=webpage.main, args=(get_track,))
+    webpage_thread = threading.Thread(target=webpage.main,
+                                      args=(get_track, shut_down))
     music_thread = threading.Thread(target=run)
     webpage_thread.start()
     music_thread.start()
-    cmd = None
-    while cmd != "quit":
-        cmd = raw_input()
-    print "shutting down..."
-    music_thread._Thread__stop()
-    webpage_thread._Thread__stop()
-    if vlc_process:
-        vlc_process.terminate()
+    if not DEBUG:
+        cmd = None
+        while cmd != "quit":
+            cmd = raw_input()
+        print "shutting down..."
+        shut_down()
 
 if __name__ == '__main__':
     main()
--- webpage.py
+++ webpage.py
@@ -1,4 +1,5 @@
 import os
+from socket import gethostbyname, gethostname
 from flask import Flask, request, render_template
 app = Flask(__name__)
 
@@ -6,23 +7,27 @@ base = os.path.abspath(os.path.curdir)
 
 @app.route('/shutdown')
 def shutdown():
-    func = request.environ.get('werkzeug.server.shutdown')
-    if func is None:
-        raise RuntimeError('Not running with the Werkzeug Server')
-    func()
+    shut_down()
+##    func = request.environ.get('werkzeug.server.shutdown')
+##    if func is None:
+##        raise RuntimeError('Not running with the Werkzeug Server')
+##    func()
 
 @app.route('/')
 def hello_world():
-    track = status()
+    track = get_track()
     try:
         return render_template('track.html', track=track)
     except Exception as e:
         print `e`
 
-def main(get_status):
-    global status
-    status = get_status
-    app.run(host='0.0.0.0', port=80)
+def main(get_track_, shut_down_):
+    global get_track, shut_down
+    get_track = get_track_
+    shut_down = shut_down_
+    ## ^-- this is fucking ugly, TODO: fix it
+    ip_addr = gethostbyname(gethostname())
+    app.run(host=ip_addr, port=80)
 
 if __name__ == '__main__':
     main(lambda: None)
